a47a56ba34aee4ab0fef2b3fd2034d1607a8eeef
Sheriefs Gamble <sgamble284@gmail.com>
2020-02-16 14:46:52 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, "M", "brown", "mild", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy',20,'M','blue','hot',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha',30,'F','red','hot',0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown',40,'M','yellow','hot',0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa',50,'M','black','pink',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch',60,'F','black','white',0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy',70,'M','black','grey',0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL,80,'M','black','magenta',1);
